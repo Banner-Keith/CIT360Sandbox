0dfec6dba4ae046ec78e94d9aebbf94723fcfca8
Keith Banner <knbanner@outlook.com>
2016-03-08 15:13:25 -0700

    SimpleThreads code optimization.

--- ParallelProcessing/src/parallelprocessing/SimpleThreads.java
+++ ParallelProcessing/src/parallelprocessing/SimpleThreads.java
@@ -13,10 +13,17 @@ import java.util.ArrayList;
  */
 public class SimpleThreads extends Thread {
 
-    static ArrayList<String> rankings = new ArrayList();
-    static int numberFinished = 0;
+    static ArrayList<String> rankings = new ArrayList<>();
     static int numberOfThreads;
     int numberOfRounds = 100;
+
+    public static void simpleThreadsExample(int numberOfRacers) {
+        numberOfThreads = numberOfRacers;
+        for (int threadCnt = 0; threadCnt < numberOfThreads; threadCnt++) {
+            SimpleThreads simpleThreads = new SimpleThreads();
+            simpleThreads.start();
+        }
+    }
 
     @Override
     public void run() {
@@ -31,38 +38,28 @@ public class SimpleThreads extends Thread {
                 }
             }
             System.out.println(sb.toString());
+            
             int sleepTime = 4 * (1 + (int) (Math.random() * 50));
             try {
                 Thread.currentThread().sleep(sleepTime);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
             if (i == numberOfRounds - 1) {
                 if (rankings.isEmpty()) {
                     rankings.add(Thread.currentThread().getName());
-                }
-                else if (rankings.get(rankings.size() - 1).length() > 0) {
+                } else {
                     rankings.add(Thread.currentThread().getName());
                 }
-                numberFinished++;
             }
         }
-        if (numberFinished == numberOfThreads) {
-            System.out.println("\nRank\tRacer"
-                    + "\n----------------");
+        if (numberOfThreads == rankings.size()) {
+            System.out.println("\nRank\tRacer\n----------------");
             for (int k = 0; k < rankings.size(); k++) {
                 System.out.println((k + 1) + ".\t" + rankings.get(k));
             }
             System.out.println("\n" + rankings.get(0) + " wins!");
-        }
-    }
-
-    public static void simpleThreadsExample(int numberOfRacers) {
-        numberOfThreads = numberOfRacers;
-        for (int threadCnt = 0; threadCnt < numberOfThreads; threadCnt++) {
-            SimpleThreads simpleThreads = new SimpleThreads();
-            simpleThreads.start();
         }
     }
 }
