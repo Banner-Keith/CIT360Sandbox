a40aeaa5ba34f557a78e80e4511907c8e94638b1
Keith Banner <knbanner@outlook.com>
2016-03-08 14:49:22 -0700

    Fixed an issue with printing out the racer progress.
    Also made several improvements to how the racer progress is displayed.
    
    Now displays the rankings of all racers rather than just the winner.

--- ParallelProcessing/src/parallelprocessing/ParallelProcessing.java
+++ ParallelProcessing/src/parallelprocessing/ParallelProcessing.java
@@ -5,17 +5,36 @@
  */
 package parallelprocessing;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Keith
  */
 public class ParallelProcessing {
+    static int numberOfRacers = 3;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SimpleThreads.simpleThreadsExample(3);
+        getNumberOfRacers();
+        SimpleThreads.simpleThreadsExample(numberOfRacers);
     }
     
+    public static void getNumberOfRacers() {
+        boolean valid = false;
+        while (!valid) {
+            System.out.println("Enter a number between 2 and 10 for how many threads you want to race.");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            if (input <= 10 && input > 1) {
+                numberOfRacers = input;
+                valid = true;
+            }
+            else {
+                System.out.println("Invalid input, number must be between 2 and 10 inclusive.");
+            }
+        }
+    }
 }
--- ParallelProcessing/src/parallelprocessing/SimpleThreads.java
+++ ParallelProcessing/src/parallelprocessing/SimpleThreads.java
@@ -1,5 +1,7 @@
 package parallelprocessing;
 
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +13,7 @@ package parallelprocessing;
  */
 public class SimpleThreads extends Thread {
 
-    static String winner = "";
+    static ArrayList<String> rankings = new ArrayList();
     static int numberFinished = 0;
     static int numberOfThreads;
     int numberOfRounds = 100;
@@ -21,7 +23,6 @@ public class SimpleThreads extends Thread {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numberOfRounds; i++) {
             sb.setLength(0);
-            //System.out.println(Thread.currentThread().getName() + ", round " + (i + 1) + " has started");
             for (int j = 0; j < numberOfRounds; j++) {
                 if (j == i) {
                     sb.append(Thread.currentThread().getName().charAt(Thread.currentThread().getName().length() - 1));
@@ -30,22 +31,30 @@ public class SimpleThreads extends Thread {
                 }
             }
             System.out.println(sb.toString());
-            int sleepTime = 1 + (int) (Math.random() * 100);
+            int sleepTime = 4 * (1 + (int) (Math.random() * 50));
             try {
                 Thread.currentThread().sleep(sleepTime);
             } catch (Exception e) {
                 e.printStackTrace();
             }
             
-            if (i == numberOfRounds - 1 && winner.equals("")) {
-                winner = Thread.currentThread().getName();
-            }
             if (i == numberOfRounds - 1) {
+                if (rankings.isEmpty()) {
+                    rankings.add(Thread.currentThread().getName());
+                }
+                else if (rankings.get(rankings.size() - 1).length() > 0) {
+                    rankings.add(Thread.currentThread().getName());
+                }
                 numberFinished++;
             }
         }
         if (numberFinished == numberOfThreads) {
-            System.out.println("\n" + winner + " wins!");
+            System.out.println("\nRank\tRacer"
+                    + "\n----------------");
+            for (int k = 0; k < rankings.size(); k++) {
+                System.out.println((k + 1) + ".\t" + rankings.get(k));
+            }
+            System.out.println("\n" + rankings.get(0) + " wins!");
         }
     }
 
